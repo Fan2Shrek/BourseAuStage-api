28825a37f0eb96c8cd90a446ab8d33adbaf17201
Kevin Fery <kevin.fery@outlook.com>
2024-04-21 01:05:37 +0200

    add fixture v1

---
+++ tests/Factory/CompanyFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Factory;
+
+use App\Entity\Company;
+use Zenstruck\Foundry\ModelFactory;
+
+/**
+ * @extends ModelFactory<Company>
+ */
+final class CompanyFactory extends ModelFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function getDefaults(): array
+    {
+        return [
+            'name' => self::faker()->company(),
+            'legalStatus' => self::faker()->randomElement(['SARL', 'SA', 'SAS', 'EI', 'EURL']),
+            'siretNumber' => self::faker()->siret(),
+            'socialLink' => self::faker()->url(),
+            'address' => self::faker()->address(),
+            'postCode' => self::faker()->postCode(),
+            'country' => self::faker()->country(),
+        ];
+    }
+
+    protected static function getClass(): string
+    {
+        return Company::class;
+    }
+}
