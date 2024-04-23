24b33fe7fb0d7edeb96f1d54e9c605958377f92e
BatMaxou <maxime@maximebatista.lycee@gmail.com>
2024-04-23 19:45:13 +0200

    :sparkles: Default link to 'my account'

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,11 +6,12 @@ use App\Entity\User;
 use App\Entity\Company;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -22,10 +23,21 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'dashboard')]
     public function index(): Response
     {
+        $user = $this->getUser();
+
+        if (!$user || !$user instanceof User) {
+            return $this->redirectToRoute('app_login');
+        }
+
         /** @var AdminUrlGenerator */
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
 
-        return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+        return $this->redirect(
+            $adminUrlGenerator->setController(UserCrudController::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId($user->getId())
+                ->generateUrl()
+        );
     }
 
     public function configureDashboard(): Dashboard
@@ -38,7 +50,7 @@ class DashboardController extends AbstractDashboardController
     {
         // A mettre si besoin d'un vrai dashboard avec chart etc...
         // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToRoute($this->translator->trans('dashboard.menu.api'), 'fas fa-layer-group', 'home');
+        yield MenuItem::linkToRoute($this->translator->trans('dashboard.menu.account'), 'fas fa-user', 'home');
         yield MenuItem::linkToCrud($this->translator->trans('dashboard.menu.users'), 'fas fa-users', User::class);
         yield MenuItem::linkToCrud($this->translator->trans('dashboard.menu.companies'), 'fas fa-building', Company::class);
     }
