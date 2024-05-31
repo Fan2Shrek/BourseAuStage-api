a2124f81cc6b093621dedeb02f4d4bb29fb8bdd1
Pierre Ambroise <pierre27.ambroise@gmail.com>
2024-05-31 12:48:59 +0200

    Add collaborator job title

--- src/Entity/Collaborator.php
+++ src/Entity/Collaborator.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\Link;
 use App\Enum\RoleEnum;
 use ApiPlatform\Metadata\Get;
 use Doctrine\ORM\Mapping as ORM;
@@ -22,6 +23,10 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
         new GetCollection(
             normalizationContext: ['groups' => ['api:collaborator:read']],
         ),
+        new GetCollection(
+            uriTemplate: '/companies/{id}/collaborators',
+            uriVariables: ['id' => new Link(fromClass: Company::class, toProperty: 'company')],
+        ),
     ],
 )]
 // EXEMPLE
@@ -40,6 +45,9 @@ class Collaborator extends User
     #[ORM\ManyToOne(targetEntity: Company::class)]
     private Company $company;
 
+    #[ORM\Column(length: 255)]
+    private ?string $jobTitle = null;
+
     public function __construct()
     {
         $this->setRoles([RoleEnum::COLLABORATOR->value]);
@@ -56,6 +64,18 @@ class Collaborator extends User
     {
         $this->company = $company;
         $this->updatedAt = new \DateTimeImmutable();
+
+        return $this;
+    }
+
+    public function getJobTitle(): ?string
+    {
+        return $this->jobTitle;
+    }
+
+    public function setJobTitle(string $jobTitle): static
+    {
+        $this->jobTitle = $jobTitle;
 
         return $this;
     }
