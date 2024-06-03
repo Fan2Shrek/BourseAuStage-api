5e4acca961272f27b235afb680c439686486d78f
BatMaxou <maximebatista.lycee@gmail.com>
2024-06-03 15:13:22 +0200

    :bug: Real date fixtures

--- tests/Factory/OfferFactory.php
+++ tests/Factory/OfferFactory.php
@@ -12,14 +12,31 @@ final class OfferFactory extends ModelFactory
 {
     protected function getDefaults(): array
     {
+        $availableAt = self::faker()->randomElement([new \DateTime('+1 month'), new \DateTime('+2 days'), new \DateTime('+7 days')]);
+        $startDate = (clone $availableAt)->add(self::faker()->randomElement([
+            \DateInterval::createFromDateString('1 day'),
+            \DateInterval::createFromDateString('2 days'),
+            \DateInterval::createFromDateString('3 days'),
+            \DateInterval::createFromDateString('4 days'),
+            \DateInterval::createFromDateString('5 days'),
+            \DateInterval::createFromDateString('6 days'),
+        ]));
+        $endDate = (clone $startDate)->add(self::faker()->randomElement([
+            \DateInterval::createFromDateString('1 week'),
+            \DateInterval::createFromDateString('2 weeks'),
+            \DateInterval::createFromDateString('1 month'),
+            \DateInterval::createFromDateString('2 months'),
+            \DateInterval::createFromDateString('6 months'),
+        ]));
+
         return [
             'isInternship' => self::faker()->boolean(),
             'isPayed' => self::faker()->boolean(),
             'name' => join(' ', self::faker()->words(3)),
-            'end' => self::faker()->dateTime(),
-            'start' => self::faker()->dateTime(),
+            'end' => $endDate,
+            'start' => $startDate,
             'company' => CompanyFactory::random(),
-            'availableAt' => self::faker()->randomElement([new \DateTime('+ 1 month'), new \DateTime('+ 2 day'), new \DateTime('+ 7 day')]),
+            'availableAt' => $availableAt,
             'description' => self::faker()->text(255),
             'studyLevel' => StudyLevelFactory::random(),
         ];
