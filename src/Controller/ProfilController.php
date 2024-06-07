e2064e4bb0ef3b43b6b4d617527b532403a13103
Pierre Ambroise <pierre27.ambroise@gmail.com>
2024-06-07 10:17:47 +0200

    add me pages

---
+++ src/Controller/ProfilController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ProfilController extends AbstractController
+{
+    public function __construct(
+        private readonly EntityManagerInterface $em,
+    ) {
+    }
+
+    #[Route('/api/me', methods: ['POST'])]
+    public function updateProfil(Request $request, #[CurrentUser] User $user): Response
+    {
+        $payload = $request->getPayload();
+
+        $user
+            ->setEmail($payload->get('email', $user->getEmail()))
+            ->setFirstName($payload->get('firstname', $user->getFirstName()))
+            ->setLastName($payload->get('lastname', $user->getLastName()))
+            ->setPhone($payload->get('phone', $user->getPhone()));
+
+        $this->em->flush();
+
+        return new Response;
+    }
+
+    #[Route('/api/me', methods: ['GET'])]
+    public function getProfil(#[CurrentUser] User $user, SerializerInterface $serializer): Response
+    {
+        $data = $serializer->serialize($user, 'json', ['groups' => ['user:read']]);
+
+        return $this->json(json_decode($data));
+    }
+}
