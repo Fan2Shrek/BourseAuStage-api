c3f00029a408347929489ca10e0f6d359e9a6c4c
Fan2Shrek <pierre27.ambroise@gmail.com>
2024-06-10 22:39:28 +0200

    Add desrialization

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -25,19 +26,19 @@ class ProfilController extends AbstractController
 
         $user
             ->setEmail($payload->get('email', $user->getEmail()))
-            ->setFirstName($payload->get('firstname', $user->getFirstName()))
-            ->setLastName($payload->get('lastname', $user->getLastName()))
+            ->setFirstName($payload->get('firstName', $user->getFirstName()))
+            ->setLastName($payload->get('lastName', $user->getLastName()))
             ->setPhone($payload->get('phone', $user->getPhone()));
 
         $this->em->flush();
 
-        return new Response;
+        return new JsonResponse;
     }
 
     #[Route('/api/me', methods: ['GET'])]
     public function getProfil(#[CurrentUser] User $user, SerializerInterface $serializer): Response
     {
-        $data = $serializer->serialize($user, 'json', ['groups' => ['user:read']]);
+        $data = $serializer->serialize($user, 'json', ['groups' => ['api:user:read']]);
 
         return $this->json(json_decode($data));
     }
